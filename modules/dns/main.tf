f7223a8bb8e0e50ece0a872a20a901267f515e99
Jessie King <jessie.kinganga@gmail.com>
2025-07-09 00:23:01 +0100

    fix formating

--- modules/dns/main.tf
+++ modules/dns/main.tf
@@ -3,5 +3,5 @@ resource "google_dns_managed_zone" "default" {
   name        = "jess-dns-app"
   dns_name    = "jess-pw-generator-app.com."
   description = "Public DNS zone"
-  visibility = "public"
+  visibility  = "public"
 }
